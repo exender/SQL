50b4e9a8a0d9c5ca7f04e5b8572568e0c93f9a8a
Biozard01 <arthur.laforest33@gmail.com>
2020-04-09 17:37:37 +0200

    c long

--- progSQL.sql
+++ progSQL.sql
@@ -1,4 +1,4 @@
-USE LOS
+﻿USE LOS
 
 
 -- CHAMPSTATS 
@@ -98,7 +98,7 @@ VALUES(004, 592, 8.5, 68, 1, 36, 32, 350, 1)
 INSERT CHAMPSTATS
 VALUES(005, 600, 5, 50, 1, 26, 34, 335, 1)
 
--- Ma�tre-Yi
+-- Maître Yi
 INSERT CHAMPSTATS
 VALUES(006, 598, 7.5, 66, 1, 33, 32, 355, 1)
 
@@ -106,6 +106,63 @@ VALUES(006, 598, 7.5, 66, 1, 33, 32, 355, 1)
 INSERT CHAMPSTATS
 VALUES(007, 560, 5.5, 56, 1, 25, 30, 335, 0)
 
+-- Nunu & Willump
+INSERT CHAMPSTATS
+VALUES(008, 570, 5, 61, 1, 32, 32, 345, 1)
 
+-- Ryze
+INSERT CHAMPSTATS
+VALUES(009, 570, 8, 55, 1, 21, 34, 340, 0)
+
+-- Sion
+INSERT CHAMPSTATS
+VALUES(010, 545, 7.5, 68, 1, 32, 32, 345, 1)
 
 SELECT * FROM CHAMPSTATS
+
+
+-- Alistar
+INSERT GAMEPLAYSTATS
+VALUES('Yokitch', 000, 1, 'Après-coup', 'Q', 0.15, 'Mid', 'Ignite', 6.16, 50.3, 0.5, 4.3, 'Support')
+
+-- Annie
+INSERT GAMEPLAYSTATS
+VALUES('Meliodas Verde', 001, 0, 'Électrocution', 'Q', 0.30, 'Late', 'Ignite', 6.36, 52.0, 0.3, 1.8, 'Middle')
+
+-- Ashe
+INSERT GAMEPLAYSTATS
+VALUES('Ganador', 002, 1, 'Tempo mortel', 'W', 0.40, 'Late', 'Heal', 6.43, 49.9, 0.5, 7.9, 'Bottom')
+
+-- Fiddlesticks
+INSERT GAMEPLAYSTATS
+VALUES('Ferrari Fiddle', 003, 0, 'Prédateur', 'W', 0.56, 'Late', 'Smite', 6.63, 48.4, 23.5, 10.1, 'Jungle')
+
+-- Jax
+INSERT GAMEPLAYSTATS
+VALUES('B1GBOPPA', 004, 1, 'Conquérant', 'E', 0.60, 'Mid', 'Teleport', 5.26, 48.8, 7.8, 10.5, 'Top')
+
+-- Kayle
+INSERT GAMEPLAYSTATS
+VALUES('kayle 1v9', 005, 1, 'Jeu de jambes', 'E', 0.30, 'Mid', 'Teleport', 5.2, 49.8, 0.3, 2.9, 'Top')
+
+-- Maître Yi
+INSERT GAMEPLAYSTATS
+VALUES('Zαpdo', 006, 1, 'Conquérant', 'Q', 0.35, 'Late', 'Smite', 6.2, 48.3, 25.2, 7.2, 'Jungle')
+
+-- Morgana
+INSERT GAMEPLAYSTATS
+VALUES('Skylandz', 007, 1, 'Invocation d"Aery', 'W', 0.15, 'Mid', 'Ignite', 6.66, 51.0, 40.0, 10.5, 'Middle')
+
+-- Nunu & Willump
+INSERT GAMEPLAYSTATS
+VALUES('Kesha', 008, 1, 'Prédateur', 'Q', 0.20, 'Early', 'Smite', 6.6, 50.8, 0.6, 3.3, 'Jungle')
+
+-- Ryze
+INSERT GAMEPLAYSTATS
+VALUES('bruv machine', 009, 0, 'Rush phasique', 'W', 0.05, 'Late', 'Teleport', 5.33, 46.2, 0.9, 4.5, 'Middle')
+
+-- Sion
+INSERT GAMEPLAYSTATS
+VALUES('Thebausffs', 010, 0, 'Poigne de l"immortel', 'Q', 0.30, 'Late', 'Teleport', 5.9, 50.4, 0.1, 2.7, 'Top')
+
+SELECT * FROM GAMEPLAYSTATS
