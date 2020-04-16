a0bb1f8bdba6a72a5e801b30917106cb9c84dbe4
Biozard01 <arthur.laforest33@gmail.com>
2020-04-16 16:47:03 +0200

    yeah

--- reqSQL.sql
+++ reqSQL.sql
@@ -2,7 +2,6 @@ USE LOS
 
 
 -- 1
-
 SELECT top(5) CONCAT(ls.CHAMP_NAME, ' a ', cs.HEALTH, ' vie et ', cs.AD, ' d"attaque') AS "Nom du champion, sa vie et son attaque"
 FROM LORESTATS ls
 INNER JOIN CHAMPSTATS cs
@@ -13,7 +12,6 @@ ORDER BY cs.HEALTH DESC, cs.AD DESC
 
 
 -- 2
-
 SELECT top(5) CONCAT(ls.CHAMP_NAME, ' et un champion ', gs.BEST_WIN_TIME, ' game et a un win rate de : ' , gs.WIN_RATE) AS "Nom du champion, son taux de victoire selon la durée de la partie et son win rate"
 FROM GAMEPLAYSTATS gs
 INNER JOIN LORESTATS ls
@@ -23,8 +21,7 @@ GROUP BY ls.CHAMP_NAME, gs.WIN_RATE, gs.BEST_WIN_TIME
 ORDER BY gs.WIN_RATE DESC, gs.BEST_WIN_TIME DESC
 
 
--- 3 
-
+-- 3
 SELECT top(5) CONCAT(ls.CHAMP_NAME, ' est ban ', gs.BAN_RATE, ' % du temps a cause d"un KDA de ', gs.KDA) AS "Nom du champion, son % de bannisement et son KDA"
 FROM GAMEPLAYSTATS gs
 INNER JOIN LORESTATS ls
@@ -34,7 +31,6 @@ ORDER BY gs.BAN_RATE DESC
 
 
 -- 4
-
 SELECT top(5) CONCAT(gs.HIGHEST_RANKED_PLAYER, ' est le joueur avec le plus haut rang sur ', ls.CHAMP_NAME, ', champion qui est a ' , gs.WIN_RATE, ' % de win rate au ' , gs.POST) AS "Meilleur joueur, nom du champion, win rate et poste"
 FROM GAMEPLAYSTATS gs
 INNER JOIN LORESTATS ls
@@ -45,7 +41,6 @@ ORDER BY gs.WIN_RATE DESC
 
 
 -- 5
-
 SELECT top(10) CONCAT(ls.CHAMP_NAME, ' a ' , cs.HEALTH, ' de vie, ', cs.AD, ' d"attaque et prend ' , i.FIRST_ITEM_BUILT, ' comme premier objet') AS "Nom du champion, sa vie, son attaque et le premier objet build"
 FROM LORESTATS ls
 INNER JOIN CHAMPSTATS cs
@@ -59,7 +54,6 @@ GROUP BY ls.CHAMP_NAME, cs.HEALTH, cs.AD, i.FIRST_ITEM_BUILT
 
 
 -- 6
-
 SELECT CONCAT(ls.CHAMP_NAME, ' a ', ms.NBR_SKIN, ' skin et a ', DATEDIFF(YEAR, ms.RELEASE_DATE, GETDATE()), ' ans') AS "Nom du champion, son nombre de skin et son ancienneté"
 FROM METASTATS ms
 INNER JOIN LORESTATS ls
@@ -70,7 +64,6 @@ HAVING DATEDIFF(YEAR, ms.RELEASE_DATE, GETDATE()) < 11
 
 
 -- 7
-
 SELECT top(5) CONCAT(ls.CHAMP_NAME, ' a ' , cs.AD, ' d"attaque et est un champion a ressources méta') AS "Nom du champion et son attaque"
 FROM LORESTATS ls
 INNER JOIN CHAMPSTATS cs
@@ -83,7 +76,6 @@ ORDER BY cs.AD DESC
 
 
 -- 8
-
 SELECT O.DRAGONS_TAKEN AS "Nombre de dragons prit dans les partie ou le red buff a été prit en premier"
 FROM OBJECTIVES O
 WHERE o.RED_BUFF_TAKEN_FIRST LIKE 1
@@ -91,7 +83,6 @@ GROUP BY o.DRAGONS_TAKEN
 
 
 -- 9
-
 SELECT top(10) CONCAT(ls.CHAMP_NAME, ' a ', gs.WIN_RATE, ' % de win rate', ' et est un champion facile coûtant moins de 3150 essences bleus ') AS "Nom du champion et son taux de victoire"
 FROM METASTATS ms
 INNER JOIN LORESTATS ls
@@ -104,13 +95,39 @@ ORDER BY gs.WIN_RATE DESC
 
 
 -- 10
---cs.HEALTH, cs.HEALTH_REGEN, cs.ARMOR,  cs.MR,  cs.AD, cs.MVT_SPEED
+SELECT top(1) CONCAT('Les ' , i.FIRST_BOOTS_BUILT,  ' sont les bottes les plus faites avec ', COUNT(i.FIRST_BOOTS_BUILT), ' bottes faites') AS "Les bottes les plus faites"
+FROM ITEMS i
+GROUP BY i.FIRST_BOOTS_BUILT
+ORDER BY  COUNT(i.FIRST_BOOTS_BUILT) DESC
 
-SELECT CONCAT(ls.CHAMP_NAME, ' a les stats les plus élever de base') AS "Nom du champion"
-FROM CHAMPSTATS cs
+
+-- 11
+SELECT top(1) CONCAT(i.FIRST_ITEM_BUILT,  ' est l"item le plus fait avec ', COUNT(i.FIRST_ITEM_BUILT), ' faits') AS "L'item le plus fait"
+FROM ITEMS i
+GROUP BY i.FIRST_ITEM_BUILT
+ORDER BY COUNT(i.FIRST_ITEM_BUILT) DESC
+
+
+-- 12 
+SELECT top(1) CONCAT(i.FIRST_ITEM_BUILT,  ' est l"item le plus fait avec ', COUNT(i.FIRST_ITEM_BUILT), ' faits') AS "L'item le plus fait sur les CAC"
+FROM ITEMS i
+INNER JOIN CHAMPSTATS cs
+ON(i.CHAMP_ID = cs.CHAMP_ID)
+WHERE cs.CAC LIKE 1
+GROUP BY i.FIRST_ITEM_BUILT
+ORDER BY COUNT(i.FIRST_ITEM_BUILT) DESC
+
+
+-- 13 
+SELECT CONCAT(ls.CHAMP_NAME, ' a un win rate de ', gs.WIN_RATE, ' % et a ', DATEDIFF(YEAR, ms.RELEASE_DATE, GETDATE()), ' ans') AS "Nom du champion, son win rate et son ancienneté"
+FROM METASTATS ms
 INNER JOIN LORESTATS ls
-ON(cs.CHAMP_ID = ls.CHAMP_ID)
-ORDER BY ls.CHAMP_NAME ASC
+ON(ms.CHAMP_ID = ls.CHAMP_ID)
+INNER JOIN GAMEPLAYSTATS gs
+ON(ms.CHAMP_ID = gs.CHAMP_ID)
+GROUP BY ms.RELEASE_DATE, ls.CHAMP_NAME, gs.WIN_RATE
+ORDER BY gs.WIN_RATE DESC
+
 
 -- (BONUS)
 
